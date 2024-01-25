21346b97b274f17ddc56ff371e3d1bfb04da7d02
prakashuniyal01 <uniyalprakash7.com>
2024-01-25 14:26:05 +0530

    compalete task js and ts

---
+++ crego-task-react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import ExpressionForm from "./components/ExpressionForm";
+
+function App() {
+  return (
+    <div className="pb-5 pt-3">
+      <h2 className="text-warning text-center mt-4 mb-5">Expression Form</h2>
+      <ExpressionForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,9 @@
+## task link 
+  --> https://rajankur.notion.site/Frontend-Developer-Assignment-React-Bootstrap-a795e32a70974439a6b73e482c0a853b
+
+
+### file  --> crego-task-react-js 
+    -> using javascript
+
+### file  --> crego-task-react-ts
+    -> using typescript
